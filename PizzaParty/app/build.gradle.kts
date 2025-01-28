d9b866cf2599d2f95d0b4df9b2eaa0212fb8832b
Xavier Garcia <xmangarcia17@gmail.com>
2025-01-28 00:19:09 -0800

    feat: refactor project to MVVM architecture (5.2)

--- PizzaParty/app/build.gradle.kts
+++ PizzaParty/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
