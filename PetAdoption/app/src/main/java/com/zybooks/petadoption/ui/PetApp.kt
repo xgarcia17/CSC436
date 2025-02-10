986df352b3007df14007fc89a72d7a34814721c5
Xavier Garcia <xmangarcia17@gmail.com>
2025-02-09 23:36:55 -0800

    feat: complete List screen

--- PetAdoption/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ PetAdoption/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -1,13 +1,27 @@
 package com.zybooks.petadoption.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zybooks.petadoption.data.Pet
+import com.zybooks.petadoption.data.PetDataSource
+import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 
 @Composable
 fun PetApp(
@@ -30,4 +44,47 @@ fun PetAppBar(
       ),
       modifier = modifier
    )
+}
+
+@Composable
+fun ListScreen(
+   petList: List<Pet>,
+   onImageClick: (Pet) -> Unit,
+   modifier: Modifier = Modifier
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Find a Friend"
+         )
+      }
+   ) { innerPadding ->
+      LazyVerticalGrid(
+         columns = GridCells.Adaptive(minSize = 128.dp),
+         contentPadding = PaddingValues(0.dp),
+         modifier = modifier.padding(innerPadding)
+      ) {
+         items(petList) { pet ->
+            Image(
+               painter = painterResource(id = pet.imageId),
+               contentDescription = "${pet.type} ${pet.gender}",
+               modifier = Modifier.clickable(
+                  onClick = { onImageClick(pet) },
+                  onClickLabel = "Select the pet"
+               )
+            )
+         }
+      }
+   }
+}
+
+@Preview
+@Composable
+fun PreviewListScreen() {
+   PetAdoptionTheme {
+      ListScreen(
+         petList = PetDataSource().loadPets(),
+         onImageClick = { }
+      )
+   }
 }
